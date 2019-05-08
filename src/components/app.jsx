17f31fda3d57c096635d0e275d84f4c70d004408
Benoit Prigent <benoit.prigent@grenoble-em.com>
2019-05-08 13:46:24 +0200

    Add GMAPS Api -> display the map

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 
+import FlatList from './flat_list.jsx';
+import flats from '../../data/flats.js';
 
-import FlatList from './flat_list.jsx'
-import flats from '../../data/flats.js'
+import GoogleMapReact from 'google-map-react';
+import Marker from './marker.jsx';
 
 
 class App extends Component {
+
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
 
   render() {
     return (
       <div>
         <FlatList flats={flats} />
         <div className="map-container">
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyCsMgddgj8A7IyB9BxM2fm5QxE584Ctdxc" }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <Marker
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+            />
+          </GoogleMapReact>
         </div>
       </div>
     );
